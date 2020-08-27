c5b1fde1efd1b7e5def76ff63adbccd566b796bf
Vinnie Mazza <vinnie.mazza@gmail.com>
2020-08-27 21:04:51 +0000

    add python3 setuptools

--- .devcontainer/scripts/awscli.sh
+++ .devcontainer/scripts/awscli.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 echo "---installing awscli---"
+apt-get install python3-setuptools
 python3 -m pip install --upgrade pip
-pip install awscli
+pip3 install awscli
 echo "---installing awscli done---"
